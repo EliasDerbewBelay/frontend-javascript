5493993971345c66de84e5f36702b9ce7bfbce27
Elias Derbew <eliasderbew1@gmail.com>
2025-09-13 21:15:46 +0300

    add readme file to the root folder

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,8 +1,18 @@
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
-  fullTimeEmployee?: boolean;
-  yearsOfExperience: number;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
   location: string;
   [key: string]: any;
 }
+
+const teacher3: Teacher = {
+  firstName: "Alx",
+  lastName: "Ethiopia",
+  fullTimeEmployee: true,
+  location: "Ethiopia",
+  contract: true,
+};
+
+console.log(teacher3)
